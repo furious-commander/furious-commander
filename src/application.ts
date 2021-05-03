0f71690849a91c66c68bbac83dd10c8cc21f8bd8
Cafe137 <aron@aronsoos.com>
2021-05-03 23:02:42 +0200

    feat: add dim formatting and customised messages

---
+++ src/application.ts
@@ -0,0 +1,6 @@
+export interface Application {
+  name: string
+  command: string
+  version: string
+  description: string
+}
--- src/printer.ts
+++ src/printer.ts
@@ -2,6 +2,7 @@ export interface Printer {
   print(text: string): void
   printError(text: string): void
   printHeading(text: string): void
+  formatDim(text: string): string
   formatImportant(text: string): string
   getGenericErrorMessage(): string
 }
@@ -11,6 +12,7 @@ export function createDefaultPrinter(): Printer {
     print: text => console.log(text),
     printError: text => console.error(text),
     printHeading: text => console.log(text),
+    formatDim: text => text,
     formatImportant: text => text,
     getGenericErrorMessage: () => 'Failed to run command!',
   }
